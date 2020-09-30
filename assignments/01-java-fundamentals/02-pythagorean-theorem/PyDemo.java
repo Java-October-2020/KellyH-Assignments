481288d5090e7b19d53effe88ccde9c9c0fa0131
Kelly Howell <worldwarwe_5@hotmail.com>
2020-09-29 22:05:36 -0500

    added PythagoreanTheorem

---
+++ assignments/01-java-fundamentals/02-pythagorean-theorem/PyDemo.java
@@ -0,0 +1,7 @@
+public class PyDemo {
+    public static void main(String[] args) {
+        PythagoreanTheorem ans  = new PythagoreanTheorem();
+        double answer = ans.calculateHypotenuse(5,3);
+        System.out.println(answer);
+    }
+}
---
+++ assignments/01-java-fundamentals/02-pythagorean-theorem/PythagoreanTheorem.java
@@ -0,0 +1,7 @@
+import java.lang.Math;
+public class PythagoreanTheorem {
+    public double calculateHypotenuse(int legA, int legB) {
+        double legC = Math.sqrt(Math.pow(legA, 2) + Math.pow(legB, 2));
+        return legC;
+    }
+}
